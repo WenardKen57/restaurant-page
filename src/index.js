6bc95ddf25cc8c94ce6cdfb2c64f371e2b3b9b05
Wenard Santos <wenardken57@gmail.com>
2025-04-27 20:26:42 +0800

    feat: add banner image and headline

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import bannerImage from './asset/resource/banner-image.jpg';
 
 const body = document.querySelector("body");
 const header = document.createElement("header");
@@ -14,6 +15,20 @@ const navAboutBtn = document.createElement("button");
 const contentContainer = document.createElement("div");
 contentContainer.setAttribute("id", "content");
 
+const leftContainer = document.createElement("div");
+leftContainer.classList.add("left-container");
+
+const rightContainer = document.createElement("div");
+rightContainer.classList.add("right-container");
+
+const headline = document.createElement("h3");
+headline.textContent = "Chill Out with the Creamiest Scoops in Town – Your Sweet Escape Awaits!";
+headline.id = "headline";
+
+const iceCreamBanner = new Image();
+iceCreamBanner.src = bannerImage;
+iceCreamBanner.id = "banner-image";
+
 navHomeBtn.textContent = "Home";
 navMenuBtn.textContent = "Menu";
 navAboutBtn.textContent = "About";
@@ -24,5 +39,9 @@ nav.appendChild(navAboutBtn);
 
 header.appendChild(logo);
 header.appendChild(nav);
+leftContainer.appendChild(headline);
+rightContainer.appendChild(iceCreamBanner);
+contentContainer.appendChild(leftContainer);
+contentContainer.appendChild(rightContainer);
 body.appendChild(header);
 body.appendChild(contentContainer);
