0caead7cf444c6f9af237d7c615cb3e7a4ea23ba
Wenard Santos <wenardken57@gmail.com>
2025-04-27 17:38:37 +0800

    feat: add css reset & js dom elements

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './style.css';
-console.log("Hello World");
+
+const body = document.querySelector("body");
+const header = document.createElement("header");
+
+const nav = document.createElement("nav");
+const navHomeBtn = document.createElement("button");
+const navMenuBtn = document.createElement("button");
+const navAboutBtn = document.createElement("button");
+
+navHomeBtn.textContent = "Home";
+navMenuBtn.textContent = "Menu";
+navAboutBtn.textContent = "About";
+
+nav.appendChild(navHomeBtn);
+nav.appendChild(navMenuBtn);
+nav.appendChild(navAboutBtn);
+
+header.appendChild(nav);
+body.appendChild(header);
